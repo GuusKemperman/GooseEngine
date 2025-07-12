b0a30b763754cc0fb0fc3076c542fe6945a94d52
GuusKemperman <guus.kemperman@gmail.com>
2025-07-12 12:51:36 +0200

    Get all functions at runtime

--- main.cpp
+++ main.cpp
@@ -6,6 +6,51 @@ import dependency_injection;
 
 #include <windows.h>
 
+
+std::vector<std::string> GetExportedFunctions(HMODULE hModule)
+{
+    // Get DOS header
+    PIMAGE_DOS_HEADER dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(hModule);
+    if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE) 
+    {
+        throw std::runtime_error{ "Invalid DOS signature" };
+    }
+
+    // Get NT headers
+    PIMAGE_NT_HEADERS ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(
+        reinterpret_cast<BYTE*>(hModule) + dosHeader->e_lfanew);
+    if (ntHeaders->Signature != IMAGE_NT_SIGNATURE) 
+    {
+        throw std::runtime_error{ "Invalid NT signature" };
+    }
+
+    // Get export directory
+    IMAGE_DATA_DIRECTORY exportDir = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+    if (exportDir.VirtualAddress == 0) 
+    {
+        throw std::runtime_error{ "No export directory found" };
+    }
+
+    PIMAGE_EXPORT_DIRECTORY exportDirectory = reinterpret_cast<PIMAGE_EXPORT_DIRECTORY>(
+        reinterpret_cast<BYTE*>(hModule) + exportDir.VirtualAddress);
+
+    DWORD* names = reinterpret_cast<DWORD*>(
+        reinterpret_cast<BYTE*>(hModule) + exportDirectory->AddressOfNames);
+    DWORD count = exportDirectory->NumberOfNames;
+
+    std::vector<std::string> ret{};
+
+    for (DWORD i = 0; i < count; ++i)
+    {
+        const char* name = reinterpret_cast<const char*>(
+        reinterpret_cast<BYTE*>(hModule) + names[i]);
+        ret.emplace_back(name);
+    }
+
+    return ret;
+}
+
+
 int main()
 {
 	HMODULE module = LoadLibraryA(R"(ModuleDLLTest\Build\x64\Debug\ModuleDLLTest.dll)");
@@ -16,6 +61,12 @@ int main()
         return 1;
     }
 
+    auto names = GetExportedFunctions(module);
+
+    for (const auto& name : names)
+    {
+        std::cout << name << std::endl;
+    }
     // Get the address of the "HelloWorld" function
     auto helloWorldFunc = reinterpret_cast<void(*)()>(
         GetProcAddress(module, "HelloWorld")
